e62d77e26b413b5a959362c0d972643521bdcb21
kharw <kharwadesonu95@gmail.com>
2025-02-15 11:54:44 +0530

    commiting my mongo code

---
+++ catalouge.sh
@@ -0,0 +1,17 @@
+dnf module disable nodejs -y
+dnf module enable nodejs:20 -y
+dnf install nodejs -y
+useradd roboshop
+cp catalogue.service /etc/systemd/system/catalogue.service
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+mkdir /app
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip
+cd /app
+unzip /tmp/catalogue.zip
+cd /app
+npm install
+systemctl daemon-reload
+systemctl enable catalogue
+systemctl start catalogue
+dnf install mongodb-mongosh -y
+mongosh --host localhost </app/db/master-data.js
