540afa5eea0468cd99f11b17442206872b4a4157
kharw <kharwadesonu95@gmail.com>
2025-02-15 12:08:06 +0530

    commiting my catalouge code

--- catalouge.sh
+++ catalouge.sh
@@ -4,8 +4,10 @@ dnf install nodejs -y
 useradd roboshop
 cp catalogue.service /etc/systemd/system/catalogue.service
 cp mongo.repo /etc/yum.repos.d/mongo.repo
-rm -rf /app
+
+rm -r /app
 mkdir /app
+
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip
 cd /app
 unzip /tmp/catalogue.zip
